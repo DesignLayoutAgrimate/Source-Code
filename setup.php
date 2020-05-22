fed36ef28a68bbdaa05c5e354b31fc151a554c79
Tuan Nguyen <nguyenvantuan9a7@gmail.com>
2020-05-22 20:24:31 +0700

    edit new

--- setup.php
+++ setup.php
@@ -11,35 +11,50 @@
 </head>
 <body >
 
-	<?php
-	function pg_connection_string_from_database_url(){
-		extract(parse_url($_ENV["DATABASE_URL"]));
-		return "user=$user password=$pass host=$host dbname=" .substr($path,1);
-	}
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4"></div>
+			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 
-		$db = pg_connect(pg_connection_string_from_database_url());
-		if(!$db){
-			echo  "Error : Unable to open database \n";
-		}else{
-			echo '<div class="alert alert-info" style="text-align:center;">';
-			echo '<strong>Opened database successfully</strong>';
-			echo '</div>';
-		}
-		
-		$sql = "CREATE TABLE IF NOT EXISTS MyAccounts (username CHAR(10) PRIMARY KEY NOT NULL,
-		password CHAR(50))";
-		
-		print "$sql \n";
-		
-		$ret = pg_query($db, $sql);
-		if(!$ret){
-			echo pg_last_error($db);
-		}else{
-			echo "Table created successfully\n";
-		}
-		
-		pg_close($db);
-	?>
+				<?php
+					function pg_connection_string_from_database_url(){
+						extract(parse_url($_ENV["DATABASE_URL"]));
+						return "user=$user password=$pass host=$host dbname=" .substr($path,1);
+					}
+
+					$db = pg_connect(pg_connection_string_from_database_url());
+					if(!$db){
+						echo  "Error : Unable to open database \n";
+					}else{
+						echo '<div class="alert alert-info" style="text-align:center;padding-top:10px;">';
+						echo '<strong>Opened database successfully</strong>';
+						echo '</div>';
+					}
+					
+					$sql = "CREATE TABLE IF NOT EXISTS MyAccounts (username CHAR(10) PRIMARY KEY NOT NULL,
+					password CHAR(50))";
+					
+					echo '<div class="alert alert-warning" style="text-align:center;padding-top:10px;">';
+					echo '<strong>$sql</strong>';
+					echo '</div>';
+					
+					$ret = pg_query($db, $sql);
+					if(!$ret){
+						echo pg_last_error($db);
+					}else{
+						
+						echo '<div class="alert alert-primary" style="text-align:center;padding-top:10px;">';
+						echo '<strong>Table created successfully</strong>';
+						echo '</div>';
+					}
+					
+					pg_close($db);
+				?>
+				
+			</div>
+			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4"></div>
+		</div>
+	</div>
 
 </body>
 </html>
